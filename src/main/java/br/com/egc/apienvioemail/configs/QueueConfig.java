c671c9e1f4003351565aa249aa3f5bbb7f6950f4
ergcarvalho <ergcarvalho@rd.com.br>
2023-01-29 13:42:02 -0300

    ajustando versão

--- src/main/java/br/com/egc/apienvioemail/configs/QueueConfig.java
+++ src/main/java/br/com/egc/apienvioemail/configs/QueueConfig.java
@@ -1,18 +1,21 @@
 package br.com.egc.apienvioemail.configs;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class QueueConfig {
 
     @Value("${queue.email}")
     private String queueEmail;
     @Bean
     public Queue myQueue() {
+        log.info("registrando a fila :" +queueEmail);
         return new Queue(queueEmail, true);
     }
 
